f22cbc1366809228e30d58c4ee13d18dbe74768a
Oliver Chase <ojchase@gmail.com>
2011-09-19 22:33:56 -0600

    Added pearson

--- project04/similarity_metrics/similarity_metrics.rb
+++ project04/similarity_metrics/similarity_metrics.rb
@@ -65,25 +65,49 @@ end
 
 def pearson(val1, val2)
   if val1.values.length != val2.values.length
-    return "Jaccard can't be calculated as objects have a different number of values!"
+    return "Pearson's Correlation Coefficient can't be calculated as objects have a different number of values!"
   else
     length = val1.values.length
-    denom = length.to_f
-    result = 0
-    for i in (0...length)
-      if(val1.values[i] == val2.values[i])
-        if(val1.values[i] != 0)
-          result = result + 1
-        end
-      end
-    end
-    result = result / denom
+    numerator = covariance(val1, val2)
+    denom = standardDeviation(val1.values) * standardDeviation(val2.values)
+    result = numerator / denom
   end
+  
+  puts "Pearson's Correlation Coefficient is #{result}!"
+end
 
-puts "Jaccard Similarity is #{result}!"
+def covariance(val1, val2)
+  if val1.values.length != val2.values.length
+    return "Covariance can't be calculated as objects have a different number of values!"
+  else
+    length = val1.values.length
+    average1 = average(val1.values)
+    average2 = average(val2.values)
+    sum = 0
+    for i in (0...length)
+      sum = sum + ((val1.values[i] - average1) * (val2.values[i] - average2))
+    end
+    
+    result = 1 / (length.to_f - 1) * sum.to_f
+  end
+end
 
-  result = "Not yet implemented"
-  puts "Pearson Correlation Coefficient is #{result}!"
+def standardDeviation(values)
+  n = values.length
+  average = average(values)
+  sumSquared = 0
+  for i in (0...n)
+    sumSquared = sumSquared + ((values[i] - average)**2)
+  end
+  result = Math.sqrt((1 / (n.to_f - 1)) * sumSquared)
+end
+
+def average(values)
+  sum = 0
+  values.each do |value|
+    sum = sum + value
+  end
+  sum.to_f / values.length
 end
 
 def cosine(val1, val2)
@@ -110,8 +134,12 @@ end
 if __FILE__ == $0
   x = ComparisonObject.new([1, 5, "fish", 2, 3])
   
+  a = ComparisonObject.new([3, 6, 0, 3, 6])
+  b = ComparisonObject.new([1, 2, 0, 1, 2])
+
   a = ComparisonObject.new([1, 0, 3, 4])
   b = ComparisonObject.new([5, 0, 7, 4])
+  
   
   functions = [:euclidean, :smc, :jaccard, :pearson, :cosine]
   functions.each do |func|
