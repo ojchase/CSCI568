8ea9282db51a2e939b0287319793fd6b35738fc0
Oliver Chase <ojchase@gmail.com>
2011-09-19 22:45:48 -0600

    A few minor refactorings

--- project04/similarity_metrics/similarity_metrics.rb
+++ project04/similarity_metrics/similarity_metrics.rb
@@ -10,30 +10,30 @@ class ComparisonObject
   end
 end
 
-def euclidean(val1, val2)
-  if val1.values.length != val2.values.length
+def euclidean(object1, object2)
+  if object1.values.length != object2.values.length
     return "Euclidean Distance can't be calculated as objects have a different number of values!"
   else
-    length = val1.values.length
+    length = object1.values.length
     result = 0
     for i in (0...length)
-      result = result + (val1.values[i]-val2.values[i])**2
+      result = result + (object1.values[i]-object2.values[i])**2
     end
     result = Math.sqrt(result)
   end
-  
+
   puts "Euclidean distance is #{result}!"
 end
 
-def smc(val1, val2)
-  if val1.values.length != val2.values.length
+def smc(object1, object2)
+  if object1.values.length != object2.values.length
     return "SMC can't be calculated as objects have a different number of values!"
   else
-    length = val1.values.length
+    length = object1.values.length
     denom = length.to_f
     result = 0
     for i in (0...length)
-      if(val1.values[i] == val2.values[i])
+      if(object1.values[i] == object2.values[i])
         result = result + 1
       end
     end
@@ -43,16 +43,16 @@ def smc(val1, val2)
   puts "SMC is #{result}!"
 end
 
-def jaccard(val1, val2)
-  if val1.values.length != val2.values.length
+def jaccard(object1, object2)
+  if object1.values.length != object2.values.length
     return "Jaccard can't be calculated as objects have a different number of values!"
   else
-    length = val1.values.length
+    length = object1.values.length
     denom = length.to_f
     result = 0
     for i in (0...length)
-      if(val1.values[i] == val2.values[i])
-        if(val1.values[i] != 0)
+      if(object1.values[i] == object2.values[i])
+        if(object1.values[i] != 0)
           result = result + 1
         end
       end
@@ -63,35 +63,44 @@ def jaccard(val1, val2)
   puts "Jaccard Similarity is #{result}!"
 end
 
-def pearson(val1, val2)
-  if val1.values.length != val2.values.length
+def pearson(object1, object2)
+  if object1.values.length != object2.values.length
     return "Pearson's Correlation Coefficient can't be calculated as objects have a different number of values!"
   else
-    length = val1.values.length
-    numerator = covariance(val1, val2)
-    denom = standardDeviation(val1.values) * standardDeviation(val2.values)
+    length = object1.values.length
+    numerator = covariance(object1.values, object2.values)
+    denom = standardDeviation(object1.values) * standardDeviation(object2.values)
     result = numerator / denom
   end
   
   puts "Pearson's Correlation Coefficient is #{result}!"
 end
 
+def cosine(object1, object2)
+  numerator = dotProduct(object1.values, object2.values)
+  denominator = Math.sqrt(dotProduct(object1.values, object1.values)) * Math.sqrt(dotProduct(object2.values, object2.values))
+  result = numerator/denominator
+  puts "Cosine distance is #{result}!"
+end
+
+#Takes two arrays and calculates their covariance
 def covariance(val1, val2)
-  if val1.values.length != val2.values.length
+  if val1.length != val2.length
     return "Covariance can't be calculated as objects have a different number of values!"
   else
-    length = val1.values.length
-    average1 = average(val1.values)
-    average2 = average(val2.values)
+    length = val1.length
+    average1 = average(val1)
+    average2 = average(val2)
     sum = 0
     for i in (0...length)
-      sum = sum + ((val1.values[i] - average1) * (val2.values[i] - average2))
+      sum = sum + ((val1[i] - average1) * (val2[i] - average2))
     end
     
     result = 1 / (length.to_f - 1) * sum.to_f
   end
 end
 
+#Takes an array of values and calculates its standard deviation
 def standardDeviation(values)
   n = values.length
   average = average(values)
@@ -102,29 +111,24 @@ def standardDeviation(values)
   result = Math.sqrt((1 / (n.to_f - 1)) * sumSquared)
 end
 
+#Takes an array of values and calculates its average
 def average(values)
   sum = 0
   values.each do |value|
     sum = sum + value
   end
-  sum.to_f / values.length
+  return sum.to_f / values.length
 end
 
-def cosine(val1, val2)
-  numerator = dotProduct(val1, val2)
-  denominator = Math.sqrt(dotProduct(val1, val1)) * Math.sqrt(dotProduct(val2, val2))
-  result = numerator/denominator
-  puts "Cosine distance is #{result}!"
-end
-
+#Takes two arrays corresponding to two vectors and calculates their dot product
 def dotProduct(vector1, vector2)
-  if vector1.values.length != vector2.values.length
+  if vector1.length != vector2.length
     return "Dot product can't be calculated as objects have a different number of values!"
   else
-    length = vector1.values.length
+    length = vector1.length
     result = 0
     for i in (0...length)
-      result = result + (vector1.values[i]*vector2.values[i])
+      result = result + (vector1[i]*vector2[i])
     end
   end
   return result
