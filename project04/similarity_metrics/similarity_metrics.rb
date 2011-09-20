9c92ca6b38719a4ea6b3f9888e23636206b86c12
Oliver Chase <ojchase@gmail.com>
2011-09-19 21:58:47 -0600

    Added cosine

--- project04/similarity_metrics/similarity_metrics.rb
+++ project04/similarity_metrics/similarity_metrics.rb
@@ -45,7 +45,7 @@ end
 
 def jaccard(val1, val2)
   if val1.values.length != val2.values.length
-    return "SMC can't be calculated as objects have a different number of values!"
+    return "Jaccard can't be calculated as objects have a different number of values!"
   else
     length = val1.values.length
     denom = length.to_f
@@ -64,13 +64,46 @@ def jaccard(val1, val2)
 end
 
 def pearson(val1, val2)
+  if val1.values.length != val2.values.length
+    return "Jaccard can't be calculated as objects have a different number of values!"
+  else
+    length = val1.values.length
+    denom = length.to_f
+    result = 0
+    for i in (0...length)
+      if(val1.values[i] == val2.values[i])
+        if(val1.values[i] != 0)
+          result = result + 1
+        end
+      end
+    end
+    result = result / denom
+  end
+
+puts "Jaccard Similarity is #{result}!"
+
   result = "Not yet implemented"
   puts "Pearson Correlation Coefficient is #{result}!"
 end
 
 def cosine(val1, val2)
-  result = "Not yet implemented"
+  numerator = dotProduct(val1, val2)
+  denominator = Math.sqrt(dotProduct(val1, val1)) * Math.sqrt(dotProduct(val2, val2))
+  result = numerator/denominator
   puts "Cosine distance is #{result}!"
+end
+
+def dotProduct(vector1, vector2)
+  if vector1.values.length != vector2.values.length
+    return "Dot product can't be calculated as objects have a different number of values!"
+  else
+    length = vector1.values.length
+    result = 0
+    for i in (0...length)
+      result = result + (vector1.values[i]*vector2.values[i])
+    end
+  end
+  return result
 end
 
 
