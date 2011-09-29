d3cd2c6154b7768b6654d5c816720b0ca1abd87e
Oliver Chase <ojchase@gmail.com>
2011-09-29 00:25:08 -0600

    Improves k optimization, uses iris dataset

--- project05/kMeans.rb
+++ project05/kMeans.rb
@@ -3,7 +3,7 @@ require 'dataObject.rb'
 
 class KMeans
   #returns the cumulative SSE from adding each of them together
-  def run(objects, k)
+  def run(objects, k, print)
     centroids = initCentroids(objects, k)
     #print(centroids)
     nochanges = false
@@ -21,24 +21,30 @@ class KMeans
 
     end
 
-    cumulativeSSE = showResults(objects, centroids)
+    cumulativeSSE = showResults(objects, centroids, print)
     return cumulativeSSE
   end
 
   #returns the from all the clusters sse
-  def showResults(objects, centroids)
+  def showResults(objects, centroids, print)
     sse = 0
-    centroids.each do |centroid|
-      puts centroid
-      puts "Includes the following points:"
-      objects.each do |obj|
-        if(obj.getCentroid == centroid)
-          puts "    #{obj}"
+    if (print)
+      centroids.each do |centroid|
+        puts centroid
+        puts "Includes the following points:"
+        objects.each do |obj|
+          if(obj.getCentroid == centroid)
+            puts "    #{obj}"
+          end
         end
+        puts("SSE: #{sse(objects, centroid)}")
+        puts
+        sse = sse + sse(objects, centroid)
       end
-      puts("SSE: #{sse(objects, centroid)}")
-      puts
-      sse = sse + sse(objects, centroid)
+    else
+      centroids.each do |centroid|
+        sse = sse + sse(objects, centroid)
+      end
     end
     return sse
   end
@@ -75,8 +81,8 @@ class KMeans
         #puts usingCentroid
         dimensionSums[i] = dimensionSums[i] / usingCentroid.to_f
       end
-      if(usingCentroid == 0) #we'll just move the centroid to the first point - better than having an empty cluster!
-        dimensionSums = objects[0].values
+      if(usingCentroid == 0) #we'll just move the centroid to a random point - better than having an empty cluster!
+        dimensionSums = objects[rand(objects.length)].values
       end
       centroid.setCoordinates(dimensionSums)
       #puts "Centroid #{centroid.getID} moved to #{centroid.getCoordinates}"
@@ -157,18 +163,170 @@ if(__FILE__ == $0)
     DataObject.new([8, 7]),
     DataObject.new([8, 8])]
 
-  lowestSSE = 100000000
-  for i in (2..10)
-    for j in (1..5) #run five times for each k
+    objects = [DataObject.new([5.1,3.5,1.4,0.2]),
+DataObject.new([4.9,3.0,1.4,0.2]),
+DataObject.new([4.7,3.2,1.3,0.2]),
+DataObject.new([4.6,3.1,1.5,0.2]),
+DataObject.new([5.0,3.6,1.4,0.2]),
+DataObject.new([5.4,3.9,1.7,0.4]),
+DataObject.new([4.6,3.4,1.4,0.3]),
+DataObject.new([5.0,3.4,1.5,0.2]),
+DataObject.new([4.4,2.9,1.4,0.2]),
+DataObject.new([4.9,3.1,1.5,0.1]),
+DataObject.new([5.4,3.7,1.5,0.2]),
+DataObject.new([4.8,3.4,1.6,0.2]),
+DataObject.new([4.8,3.0,1.4,0.1]),
+DataObject.new([4.3,3.0,1.1,0.1]),
+DataObject.new([5.8,4.0,1.2,0.2]),
+DataObject.new([5.7,4.4,1.5,0.4]),
+DataObject.new([5.4,3.9,1.3,0.4]),
+DataObject.new([5.1,3.5,1.4,0.3]),
+DataObject.new([5.7,3.8,1.7,0.3]),
+DataObject.new([5.1,3.8,1.5,0.3]),
+DataObject.new([5.4,3.4,1.7,0.2]),
+DataObject.new([5.1,3.7,1.5,0.4]),
+DataObject.new([4.6,3.6,1.0,0.2]),
+DataObject.new([5.1,3.3,1.7,0.5]),
+DataObject.new([4.8,3.4,1.9,0.2]),
+DataObject.new([5.0,3.0,1.6,0.2]),
+DataObject.new([5.0,3.4,1.6,0.4]),
+DataObject.new([5.2,3.5,1.5,0.2]),
+DataObject.new([5.2,3.4,1.4,0.2]),
+DataObject.new([4.7,3.2,1.6,0.2]),
+DataObject.new([4.8,3.1,1.6,0.2]),
+DataObject.new([5.4,3.4,1.5,0.4]),
+DataObject.new([5.2,4.1,1.5,0.1]),
+DataObject.new([5.5,4.2,1.4,0.2]),
+DataObject.new([4.9,3.1,1.5,0.1]),
+DataObject.new([5.0,3.2,1.2,0.2]),
+DataObject.new([5.5,3.5,1.3,0.2]),
+DataObject.new([4.9,3.1,1.5,0.1]),
+DataObject.new([4.4,3.0,1.3,0.2]),
+DataObject.new([5.1,3.4,1.5,0.2]),
+DataObject.new([5.0,3.5,1.3,0.3]),
+DataObject.new([4.5,2.3,1.3,0.3]),
+DataObject.new([4.4,3.2,1.3,0.2]),
+DataObject.new([5.0,3.5,1.6,0.6]),
+DataObject.new([5.1,3.8,1.9,0.4]),
+DataObject.new([4.8,3.0,1.4,0.3]),
+DataObject.new([5.1,3.8,1.6,0.2]),
+DataObject.new([4.6,3.2,1.4,0.2]),
+DataObject.new([5.3,3.7,1.5,0.2]),
+DataObject.new([5.0,3.3,1.4,0.2]),
+DataObject.new([7.0,3.2,4.7,1.4]),
+DataObject.new([6.4,3.2,4.5,1.5]),
+DataObject.new([6.9,3.1,4.9,1.5]),
+DataObject.new([5.5,2.3,4.0,1.3]),
+DataObject.new([6.5,2.8,4.6,1.5]),
+DataObject.new([5.7,2.8,4.5,1.3]),
+DataObject.new([6.3,3.3,4.7,1.6]),
+DataObject.new([4.9,2.4,3.3,1.0]),
+DataObject.new([6.6,2.9,4.6,1.3]),
+DataObject.new([5.2,2.7,3.9,1.4]),
+DataObject.new([5.0,2.0,3.5,1.0]),
+DataObject.new([5.9,3.0,4.2,1.5]),
+DataObject.new([6.0,2.2,4.0,1.0]),
+DataObject.new([6.1,2.9,4.7,1.4]),
+DataObject.new([5.6,2.9,3.6,1.3]),
+DataObject.new([6.7,3.1,4.4,1.4]),
+DataObject.new([5.6,3.0,4.5,1.5]),
+DataObject.new([5.8,2.7,4.1,1.0]),
+DataObject.new([6.2,2.2,4.5,1.5]),
+DataObject.new([5.6,2.5,3.9,1.1]),
+DataObject.new([5.9,3.2,4.8,1.8]),
+DataObject.new([6.1,2.8,4.0,1.3]),
+DataObject.new([6.3,2.5,4.9,1.5]),
+DataObject.new([6.1,2.8,4.7,1.2]),
+DataObject.new([6.4,2.9,4.3,1.3]),
+DataObject.new([6.6,3.0,4.4,1.4]),
+DataObject.new([6.8,2.8,4.8,1.4]),
+DataObject.new([6.7,3.0,5.0,1.7]),
+DataObject.new([6.0,2.9,4.5,1.5]),
+DataObject.new([5.7,2.6,3.5,1.0]),
+DataObject.new([5.5,2.4,3.8,1.1]),
+DataObject.new([5.5,2.4,3.7,1.0]),
+DataObject.new([5.8,2.7,3.9,1.2]),
+DataObject.new([6.0,2.7,5.1,1.6]),
+DataObject.new([5.4,3.0,4.5,1.5]),
+DataObject.new([6.0,3.4,4.5,1.6]),
+DataObject.new([6.7,3.1,4.7,1.5]),
+DataObject.new([6.3,2.3,4.4,1.3]),
+DataObject.new([5.6,3.0,4.1,1.3]),
+DataObject.new([5.5,2.5,4.0,1.3]),
+DataObject.new([5.5,2.6,4.4,1.2]),
+DataObject.new([6.1,3.0,4.6,1.4]),
+DataObject.new([5.8,2.6,4.0,1.2]),
+DataObject.new([5.0,2.3,3.3,1.0]),
+DataObject.new([5.6,2.7,4.2,1.3]),
+DataObject.new([5.7,3.0,4.2,1.2]),
+DataObject.new([5.7,2.9,4.2,1.3]),
+DataObject.new([6.2,2.9,4.3,1.3]),
+DataObject.new([5.1,2.5,3.0,1.1]),
+DataObject.new([5.7,2.8,4.1,1.3]),
+DataObject.new([6.3,3.3,6.0,2.5]),
+DataObject.new([5.8,2.7,5.1,1.9]),
+DataObject.new([7.1,3.0,5.9,2.1]),
+DataObject.new([6.3,2.9,5.6,1.8]),
+DataObject.new([6.5,3.0,5.8,2.2]),
+DataObject.new([7.6,3.0,6.6,2.1]),
+DataObject.new([4.9,2.5,4.5,1.7]),
+DataObject.new([7.3,2.9,6.3,1.8]),
+DataObject.new([6.7,2.5,5.8,1.8]),
+DataObject.new([7.2,3.6,6.1,2.5]),
+DataObject.new([6.5,3.2,5.1,2.0]),
+DataObject.new([6.4,2.7,5.3,1.9]),
+DataObject.new([6.8,3.0,5.5,2.1]),
+DataObject.new([5.7,2.5,5.0,2.0]),
+DataObject.new([5.8,2.8,5.1,2.4]),
+DataObject.new([6.4,3.2,5.3,2.3]),
+DataObject.new([6.5,3.0,5.5,1.8]),
+DataObject.new([7.7,3.8,6.7,2.2]),
+DataObject.new([7.7,2.6,6.9,2.3]),
+DataObject.new([6.0,2.2,5.0,1.5]),
+DataObject.new([6.9,3.2,5.7,2.3]),
+DataObject.new([5.6,2.8,4.9,2.0]),
+DataObject.new([7.7,2.8,6.7,2.0]),
+DataObject.new([6.3,2.7,4.9,1.8]),
+DataObject.new([6.7,3.3,5.7,2.1]),
+DataObject.new([7.2,3.2,6.0,1.8]),
+DataObject.new([6.2,2.8,4.8,1.8]),
+DataObject.new([6.1,3.0,4.9,1.8]),
+DataObject.new([6.4,2.8,5.6,2.1]),
+DataObject.new([7.2,3.0,5.8,1.6]),
+DataObject.new([7.4,2.8,6.1,1.9]),
+DataObject.new([7.9,3.8,6.4,2.0]),
+DataObject.new([6.4,2.8,5.6,2.2]),
+DataObject.new([6.3,2.8,5.1,1.5]),
+DataObject.new([6.1,2.6,5.6,1.4]),
+DataObject.new([7.7,3.0,6.1,2.3]),
+DataObject.new([6.3,3.4,5.6,2.4]),
+DataObject.new([6.4,3.1,5.5,1.8]),
+DataObject.new([6.0,3.0,4.8,1.8]),
+DataObject.new([6.9,3.1,5.4,2.1]),
+DataObject.new([6.7,3.1,5.6,2.4]),
+DataObject.new([6.9,3.1,5.1,2.3]),
+DataObject.new([5.8,2.7,5.1,1.9]),
+DataObject.new([6.8,3.2,5.9,2.3]),
+DataObject.new([6.7,3.3,5.7,2.5]),
+DataObject.new([6.7,3.0,5.2,2.3]),
+DataObject.new([6.3,2.5,5.0,1.9]),
+DataObject.new([6.5,3.0,5.2,2.0]),
+DataObject.new([6.2,3.4,5.4,2.3]),
+DataObject.new([5.9,3.0,5.1,1.8])]
+
+  lowestSSE = 10000000
+  for i in (2..Math.log(objects.length)) #we don't want more than one cluster for every three points!
+    cumulativeSSE = 0
+    for j in (1..10) #run 10 times for each k
       kMeans = KMeans.new()
-      tempSSE = kMeans.run(objects, i)
-      if(tempSSE < lowestSSE)
-        k = j
-        lowestSSE = tempSSE
+      cumulativeSSE = cumulativeSSE + kMeans.run(objects, i, false)
+      if(cumulativeSSE/10.to_f() < lowestSSE)
+        k = i
       end
     end
   end
 
   puts "Ideal K is #{k}!"
+  kMeans.run(objects, k, true)
 
 end
