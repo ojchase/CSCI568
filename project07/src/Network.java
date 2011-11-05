093c828236082886966a30d8266d8b827d34bbe8
Oliver Chase <ojchase@gmail.com>
2011-11-05 00:01:11 -0600

    evidently Network hadn't been added; I don't like git

---
+++ project07/src/Network.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+public class Network
+{
+  private List<Neuron> inputNeurons = Arrays.asList(new Neuron("input1"), new Neuron("input2"), new Neuron("input3"));
+  private List<Neuron> hiddenNeurons = Arrays.asList(new Neuron("hidden1"), new Neuron("hidden2"));
+  private List<Neuron> outputNeurons = Arrays.asList(new Neuron("output1"), new Neuron("output2"), new Neuron("output3"));
+  private Queue<Neuron> neuronQueue = new LinkedList<Neuron>();
+  
+  public Network()
+  {    
+    for(Neuron n : inputNeurons)
+    {
+      for(Neuron m : hiddenNeurons)
+      {
+        new Axon(n, m);
+      }
+    }
+
+    for(Neuron n : hiddenNeurons)
+    {
+      for(Neuron m : outputNeurons)
+      {
+        new Axon(n, m);
+      }
+    }
+
+    for(Neuron n : outputNeurons)
+    {
+        new Axon(n, null);
+    }
+    
+    inputNeurons.get(0).receiveSignal(1);
+    inputNeurons.get(1).receiveSignal(0.25);
+    inputNeurons.get(2).receiveSignal(-0.5);
+    
+    neuronQueue.addAll(inputNeurons);
+    while(!neuronQueue.isEmpty())
+    {
+      Neuron firingNeuron = neuronQueue.remove();
+      List<Neuron> newNeuronsToFire = firingNeuron.fire();
+      for(Neuron n : newNeuronsToFire)
+      {
+        if(!neuronQueue.contains(n))
+        {
+          neuronQueue.add(n);
+        }
+      }
+    }
+  }
+  
+  public static void main(String[] args)
+  {
+    Network n = new Network();
+  }
+}
