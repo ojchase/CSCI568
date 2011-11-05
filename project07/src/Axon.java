d8d08231fdb2773967dbeb67265fc5cdd07cafd2
Oliver Chase <ojchase@gmail.com>
2011-11-04 22:05:46 -0600

    Axon weight updating

--- project07/src/Axon.java
+++ project07/src/Axon.java
@@ -3,27 +3,45 @@ public class Axon
   private final Neuron source;
   private final Neuron target;
   private double weight;
-  
+
   private Axon(Neuron source, Neuron target)
   {
     this.source = source;
     this.target = target;
     this.weight = Math.random() - Math.random(); // gives a range of -1 to +1
   }
-  
+
   public Neuron getSource()
   {
     return source;
   }
+
   public Neuron getTarget()
   {
     return target;
   }
+
   public double getWeight()
   {
     return weight;
   }
-  
-  
+
+  // this may or may not get used; I haven't decided how I want to update weights yet
+  private void setWeight(double weight)
+  {
+    this.weight = weight;
+  }
+
+  // this may or may not get used; I haven't decided how I want to update weights yet
+  private void addWeight(double weight)
+  {
+    this.weight += weight;
+  }
+
+  // this may or may not get used; I haven't decided how I want to update weights yet
+  private void multiplyWeight(double weight)
+  {
+    this.weight *= weight;
+  }
 
 }
