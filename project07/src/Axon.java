12bcc53908a86bcd3396e24f881ea77d25b7114e
Oliver Chase <ojchase@gmail.com>
2011-11-04 22:00:29 -0600

    Axons

--- project07/src/Axon.java
+++ project07/src/Axon.java
@@ -1,5 +1,29 @@
-
 public class Axon
 {
+  private final Neuron source;
+  private final Neuron target;
+  private double weight;
+  
+  private Axon(Neuron source, Neuron target)
+  {
+    this.source = source;
+    this.target = target;
+    this.weight = Math.random() - Math.random(); // gives a range of -1 to +1
+  }
+  
+  public Neuron getSource()
+  {
+    return source;
+  }
+  public Neuron getTarget()
+  {
+    return target;
+  }
+  public double getWeight()
+  {
+    return weight;
+  }
+  
+  
 
 }
